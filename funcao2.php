56fd5a739bdfaa9398731fc11897f5bc6739e4d0
Luan <ribeirociniluanhenry@gmail.com>
2025-08-16 13:39:58 -0300

    exercicios php

---
+++ funcao2.php
@@ -0,0 +1,12 @@
+<?php
+
+function boasVindas(){
+
+$nome = (readline("informe seu nome: "));
+$idade = (readline("qual a sua idade? "));
+$cidade = (readline("qual sua cidade? "));
+
+echo"seja bem vindo(a) $nome, de $idade anos e mora na cidade de $cidade!";
+}
+
+boasVinda();
